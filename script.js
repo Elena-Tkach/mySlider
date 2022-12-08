aac29202c332df3e1088669494f9151578ac43a9
‘Elena <tcachelena@gmail.com>
2022-12-08 10:19:45 +0600

    fix: Added slide renderer

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
 const track = document.querySelector('.js-sliders');
-const items = document.querySelectorAll('.js-slide');
 const container = document.querySelector('.js-slider-container');
+const btnsContainer = document.querySelector('.js-btns');
 const btnPrev = document.querySelector('.js-btn-prev');
 const btnNext = document.querySelector('.js-btn-next');
+
+
 let position = 0;
-const slidesToSchow = 3;
+const itemsCount = 10;
+const slidesToSchow = 2;
 const slidesToScroll = 2;
-const itemsCount = items.length;
+
 //получаем ширину каждого элемента видного в окне
 const itemWidth = container.clientWidth / slidesToSchow;
 const movePosition = slidesToScroll * itemWidth;
 
-//присваеваем эту ширину слайду 
-items.forEach((item) => {
-  item.style.cssText = `min-width:${itemWidth}px;`;
-})
+const createItem = (item) => {
+  const template = document.querySelector('#template-slide');
+  const cloneTemplate = template.content.cloneNode(true);
+
+  const itemTitle = cloneTemplate.querySelector('.js-item-title');
+  itemTitle.textContent = `${item}`;
+  return cloneTemplate;
+}
+
+const renderItem = (itemsCount) => {
+  for (let i = 1; i <= itemsCount; i++) {
+    const item = createItem(i);
+    track.append(item);
+  }
+}
+
+//присваеваем эту ширину слайду
+const giveWidthSlides = () => {
+  const items = document.querySelectorAll('.js-slide');
+  items.forEach((item) => {
+    item.style.cssText = `min-width:${itemWidth}px;`;
+  })
+}
+
+
+
+renderItem(itemsCount);
+giveWidthSlides();
+
+
 
 btnPrev.addEventListener('click', () => {
   const itemsLeft = Math.abs(position) / itemWidth;
@@ -28,17 +57,24 @@ btnNext.addEventListener('click', () => {
   position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
   setPosition();
   checkBtns();
-
 })
+
 
 const setPosition = () => {
   track.style.transform = `translateX(${position}px)`;
 }
+
 const checkBtns = () => {
   //количество слайдом - количество видимых слайдом * на ширину слайда
   btnPrev.disabled = position >= 0;
-  console.log(position);
+
   btnNext.disabled = position <= -(itemsCount - slidesToSchow) * itemWidth;
 
 }
+
 checkBtns();
+
+
+
+
+
